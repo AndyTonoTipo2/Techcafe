a6894a7a5b129d27ff69a03156b2c9ee767db080
AndyTonoTipo2 <andres.rojas1816@alumnos.udg.mx>
2024-08-29 11:25:55 -0600

    p4 signup

--- techcafe.py
+++ techcafe.py
@@ -4,10 +4,15 @@ techcafeApp = Flask(__name__)
 
 @techcafeApp.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
+
+@techcafeApp.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     techcafeApp.run(port=3300,debug=True)
+
 
 
     
