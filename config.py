0927313f029860046074f127651b1619d65921e6
AndyTonoTipo2 <andres.rojas1816@alumnos.udg.mx>
2024-09-05 11:46:59 -0600

    p6 sql

---
+++ config.py
@@ -0,0 +1,10 @@
+class Config:
+    SECRET_KEY  = 'fuhjfgijtcdryurditfitruidryur'
+    DEBUG       = True
+
+class DevelopmentConfig:
+    MYSQL_HOST      = 'localhost'
+    MYSQL_USER      = 'root'
+    MYSQL_PASSWORD  = ''
+    MYSQL_DB        = 'techcafe'
+        
