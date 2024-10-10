b41bd1171ea6d324c98f07b6d9d572436cf2b2f0
AndyTonoTipo2 <andres.rojas1816@alumnos.udg.mx>
2024-10-10 12:14:10 -0600

    p9 signout

--- models/ModelUser.py
+++ models/ModelUser.py
@@ -1,6 +1,7 @@
 from models.entities.User import User
 
 class ModelUser:
+    @classmethod
     def signin(self, db, usuario):
         try:
             selUsuario = db.connection.cursor()
@@ -8,12 +9,13 @@ class ModelUser:
             u = selUsuario.fetchone()
 
             if u is not None:
-                return User(u[0], u[1], u[2], User.validarClave(u[3],usuario.clave),u[4],u[5])
+                return User(u[0], u[1], u[2], User.validarClaves(u[3],usuario.clave),u[4],u[5])
             else:
                 return None
         except Exception as ex:
             raise Exception(ex)
         
+    @classmethod    
     def get_by_id(self, db , id):
         try:
             selUsuario = db.connection.cursor()
--- techcafe.py
+++ techcafe.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_mysqldb import MySQL
+from flask_login import LoginManager, login_user, logout_user
 from werkzeug.security import generate_password_hash
 import datetime
 from config import config
-from.models.ModelUser import ModelUser
-from.models.entities.User import User
-
-
+from models.ModelUser import ModelUser
+from models.entities.User import User
 
 techcafeApp = Flask(__name__)
 db          = MySQL(techcafeApp)
+adminSesion = LoginManager(techcafeApp)
+
+@adminSesion.user_loader
+def cargarUsuario():
+    return ModelUser.get_by_id(db, id) 
 
 @techcafeApp.route('/')
 def home():
@@ -23,16 +27,37 @@ def signup():
         clave = request.form['clave']
         claveCifrada = generate_password_hash(clave)
         fechareg = datetime.datetime.now()
-        regUsuario = db.connection.cursor()
-        regUsuario.execute("INSERT INTO usuario (nombre, correo, clave, fechareg)VALUES(%s,%s,%s,%s)",(nombre, correo, claveCifrada, fechareg))
+        reUsuario = db.connection.cursor()
+        reUsuario.execute("INSERT INTO usuario (nombre, correo, clave, fechareg)VALUES(%s,%s,%s,%s)",(nombre, correo, claveCifrada, fechareg))
         db.connection.commit()
         return render_template('home.html') 
     else: 
         return render_template('signup.html')
 
-@techcafeApp.route('/signin')
+@techcafeApp.route('/signin', methods=['GET','POST'])
 def signin():
-    return render_template('signin.html')
+    if request.method == 'POST':
+        usuario = User(0, None, request.form['correo'], request.form['clave'], None, None)
+        usuarioAutenticado = ModelUser.signin(db,usuario)
+        if usuarioAutenticado is not None:
+            login_user(usuarioAutenticado)
+            if usuarioAutenticado.clave:
+                if usuarioAutenticado.perfil == 'A':
+                    return render_template('admin.html')
+                else:
+                    return render_template('user.html')
+            else:
+                return 'Contraseña Incorrecta'
+        else:
+            return 'Correo no encontrado'        
+    else:
+        return render_template('signin.html')
+
+@techcafeApp.route('/signout',methods=['GET','POST'])
+def signout():
+    logout_user() 
+    return render_template('home.html')
+
 
 if __name__ == '__main__':
     techcafeApp.config.from_object(config['development'])
