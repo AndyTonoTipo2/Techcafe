11d9f3653733b9c9342a9a0ab2e7a41e03995287
AndyTonoTipo2 <andres.rojas1816@alumnos.udg.mx>
2024-10-24 11:31:15 -0600

    p12 CRUD

--- techcafe.py
+++ techcafe.py
@@ -84,7 +84,29 @@ def iUsuario():
     flash('Usuario Creado')
     return redirect('/sUsuario')
 
+@techcafeApp.route('/uUsuario/<int:id>',methods=['GET','POST'])
+def uUsuario(id):
+    nombre = request.form['nombre'] 
+    correo = request.form['correo']
+    clave = request.form['clave']
+    claveCifrada = generate_password_hash(clave)
+    fechareg = datetime.datetime.now()
+    perfil = request.form['perfil']
+
+    editarUsuario = db.connection.cursor()
+    editarUsuario.execute("UPDATE usuario SET nombre=%s, correo=%s, clave=%s, fechareg=%s, perfil=%s WHERE id=%s", (nombre, correo, claveCifrada, fechareg, perfil, id))
+    db.connection.commit()
+    flash('Usuario Editado')
+    return redirect('/sUsuario')
+
+@techcafeApp.route('/dUsuario/<int:id>', methods=['GET', 'POST'])
+def dUsuario(id):
     
+        eliminarUsuario = db.connection.cursor()
+        eliminarUsuario.execute("DELETE FROM usuario WHERE id=%s", (id,))
+        db.connection.commit()
+        flash('User deleted successfully')
+        return redirect('/sUsuario')
 
 if __name__ == '__main__':
     techcafeApp.config.from_object(config['development'])
