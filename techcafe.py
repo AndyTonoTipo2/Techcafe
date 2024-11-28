6f74127c20d33374d5fe7b0ef401c054d9dc6ef0
AndyTonoTipo2 <andres.rojas1816@alumnos.udg.mx>
2024-11-28 11:37:57 -0600

    A 1 PASO WE

--- techcafe.py
+++ techcafe.py
@@ -36,6 +36,7 @@ def home():
 def admin():
     return render_template('admin.html')
 
+
 @techcafeApp.route('/user')
 def user():
     productos = obtener_productos()
@@ -67,16 +68,16 @@ def signup():
 def signin():
     if request.method == 'POST':
         usuario = User(0, None, request.form['correo'], request.form['clave'], None, None)
-        usuarioAutenticado = ModelUser.signin(db,usuario)
+        usuarioAutenticado = ModelUser.signin(db, usuario)
         if usuarioAutenticado is not None:
             login_user(usuarioAutenticado)
             session['NombreU'] = usuarioAutenticado.nombre
-            session['PrefilU'] = usuarioAutenticado.perfil
+            session['PerfilU'] = usuarioAutenticado.perfil
             if usuarioAutenticado.clave:
                 if usuarioAutenticado.perfil == 'A':
-                    return render_template('admin.html')
+                    return redirect(url_for('admin')) 
                 else:
-                    return render_template('user.html')
+                    return redirect(url_for('user'))  
             else:
                 flash('Contraseña incorrecta')
                 return redirect(request.url)
@@ -90,6 +91,52 @@ def signin():
 def signout():
     logout_user() 
     return render_template('home.html')
+
+@techcafeApp.route('/sUsuario',methods=['GET','POST'])
+def sUsuario():
+    selUsuario = db.connection.cursor()
+    selUsuario.execute("SELECT * FROM usuario")
+    u = selUsuario.fetchall()
+    selUsuario.close()
+    return render_template('usuarios.html', usuarios=u)
+
+@techcafeApp.route('/iUsuario',methods=['GET','POST'])
+def iUsuario():
+    nombre = request.form['nombre'] 
+    correo = request.form['correo']
+    clave = request.form['clave']
+    claveCifrada = generate_password_hash(clave)
+    fechareg = datetime.datetime.now()
+    perfil = request.form['perfil']
+
+    crearUsuario = db.connection.cursor()
+    crearUsuario.execute("INSERT INTO usuario (nombre, correo, clave, fechareg, perfil) VALUES (%s,%s,%s,%s,%s)",(nombre, correo, claveCifrada, fechareg, perfil))
+    db.connection.commit()
+    flash('Usuario Creado')
+    return redirect('/sUsuario')
+
+@techcafeApp.route('/uUsuario/<int:id>',methods=['GET','POST'])
+def uUsuario(id):
+    nombre = request.form['nombre'] 
+    correo = request.form['correo']
+    clave = request.form['clave']
+    claveCifrada = generate_password_hash(clave)
+    fechareg = datetime.datetime.now()
+    perfil = request.form['perfil']
+
+    editarUsuario = db.connection.cursor()
+    editarUsuario.execute("UPDATE usuario SET nombre=%s, correo=%s, clave=%s, fechareg=%s, perfil=%s WHERE id=%s", (nombre, correo, claveCifrada, fechareg, perfil, id))
+    db.connection.commit()
+    flash('Usuario Editado')
+    return redirect('/sUsuario')
+
+@techcafeApp.route('/dUsuario/<int:id>', methods=['GET', 'POST'])
+def dUsuario(id):
+    eliminarUsuario = db.connection.cursor()
+    eliminarUsuario.execute("DELETE FROM usuario WHERE id=%s", (id,))
+    db.connection.commit()
+    flash('Usuario eliminado exitosamente')
+    return redirect('/sUsuario')
 
 @techcafeApp.route('/sProducto', methods=['GET', 'POST'])
 def sProducto():
@@ -160,7 +207,7 @@ def dProducto(id):
     cursor.execute("DELETE FROM productos WHERE id_producto = %s", (id,))
     db.connection.commit()
     flash('Producto eliminado exitosamente.')
-    return redirect('/sProducto')  # Redirige a la vista de usuario después de eliminar el producto
+    return redirect('/sProducto') 
 
 if __name__ == '__main__':
     techcafeApp.config.from_object(config['development'])
